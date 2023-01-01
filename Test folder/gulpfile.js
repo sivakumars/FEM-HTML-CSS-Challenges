6206f7129e8d1ff1f0b8aee873b8e46263ffa787
sivakumars <5514983+sivakumars@users.noreply.github.com>
2023-01-01 16:24:15 +0530

    commiting the changes from subfolder of a repository

--- Test folder/gulpfile.js
+++ Test folder/gulpfile.js
@@ -5,7 +5,7 @@ const cleanCss = require('gulp-clean-css');
 // Concat and minify CSS files
 gulp.task('build-css', () => {
   return gulp.src('src/styles/**/*.css')
-  .pipe(concat('styles.css'))
+  .pipe(concat('./styles.css'))
   .pipe(cleanCss())
   .pipe(gulp.dest('dist/css'));
 });
